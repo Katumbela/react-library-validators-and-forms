a85aaa9c8370e7172dd7921b45d7c32ec2096c3d
Joao Afonso Katombela <ja3328173@gmail.com>
2024-08-06 08:00:22 +0100

    chore: create lib exporter

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+
+export { default as FadeIn } from './dist/src/components/Animations/FadeIn';
+export { default as SlideUp } from './dist/src/components/Animations/SlideUp';
+export { validateEmail, validatePassword, validatePhoneNumber } from './dist/src/utils/validation';
+
